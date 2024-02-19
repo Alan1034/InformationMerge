da3735b76b741db289085a1183936bdf9ddcc59e
Chendeli <419287484@qq.com>
2024-02-19 10:40:02 +0800

    更新插件框架

--- src/InformationMergeVue3.vue
+++ src/InformationMergeVue3.vue
@@ -1,14 +1,14 @@
 <!--
  * @Author: 陈德立*******419287484@qq.com
  * @Date: 2021-10-12 17:54:01
- * @LastEditTime: 2021-11-18 16:49:22
+ * @LastEditTime: 2024-02-19 10:39:29
  * @LastEditors: 陈德立*******419287484@qq.com
  * @Github: https://github.com/Alan1034
  * @Description: 
  * @FilePath: \InformationMerge\src\InformationMergeVue3.vue
  * 
 -->
-<script>
+<script setup>
 import { copyOrderNo, style } from "./components/copy";
 
 const InformationMerge = (props, context) => {
@@ -38,7 +38,10 @@ const InformationMerge = (props, context) => {
         );
         return value ? (
           <div key={key}>
-            <span>{label}：</span>
+            <span>
+              {label}
+              {label ? "：" : ""}
+            </span>
             <el-tooltip placement="top" v-slots={slots}>
               {int}
             </el-tooltip>
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 /*
  * @Author: 陈德立*******419287484@qq.com
  * @Date: 2021-10-12 17:54:01
- * @LastEditTime: 2021-11-18 10:56:43
+ * @LastEditTime: 2024-02-19 09:36:51
  * @LastEditors: 陈德立*******419287484@qq.com
  * @Github: https://github.com/Alan1034
  * @Description: InformationMerge index
- * @FilePath: \InformationMerge\src\index.js
+ * @FilePath: \InformationMerge\src\index.ts
  * 
  */
 import * as Vue from "vue";
@@ -13,11 +13,10 @@ import * as Vue from "vue";
 import InformationMergeVue3 from './InformationMergeVue3.vue';
 import InformationMergeVue2 from './InformationMergeVue2.vue';
 
-const version = Vue.default
-  ? Number(Vue.default.version.split(".")[0])
-  : Number(Vue.version.split(".")[0]);
+// @ts-ignore
+const version = Vue.default ? Number(Vue.default.version.split(".")[0]) : Number(Vue.version.split(".")[0]);
 
-let InformationMerge = null
+let InformationMerge: any = null
 
 if (`${version}` === "3") {
   InformationMerge = InformationMergeVue3
